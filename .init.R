68a147ba83b6924e9883b221f5ea5ae77476237f
hyginn <boris.steipe@utoronto.ca>
2016-06-06 22:28:11 -0400

    Install files

---
+++ .init.R
@@ -0,0 +1,8 @@
+# .init.R
+# Functions to initialize this Exercise session
+# Boris Steipe
+# ====================================================================
+
+file.edit("R_EDA-DimensionReduction.R")
+
+# [End]
